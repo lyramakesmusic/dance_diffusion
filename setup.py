b65329028f7897b6d7808faf7fe54a6db0652f83
Lyra C <lyramakesmusic@gmail.com>
2023-05-22 13:09:18 -0700

    this is broken (no reqs)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dance_diffusion",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "",
+    ],
+)

2e5a2ff842ff6366cd281d7e0a9ceec4e874621d
Lyra C <lyramakesmusic@gmail.com>
2023-05-22 13:13:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="dance_diffusion",
-    version="0.1.0",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
-        "",
+        "torch",
+        "torchaudio",
+        "einops",
+        "scipy",
+        "k-diffusion",
+        "v-diffusion-pytorch",
+        "sample-generator",
     ],
 )
